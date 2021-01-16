ba78d6ea704cfd7ec09baa2ba690047eae143bb6
TANVIR ISLAM <mdtanvirislams@gmail.com>
2021-01-17 02:52:30 +0600

    create sample users in mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs'
 const data = {
+  users: [
+    {
+      name: 'Tanvir',
+      email: 'admin@admin.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Niloy',
+      email: 'user@user.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
     products: [
       {
         _id: '1',
